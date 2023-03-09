d42994e824e865f1e7ef78510a4471bafd8f0db7
ratoncaton <ratoncaton12@gmail.com>
2023-03-09 20:51:02 +0100

    mi primer commit

---
+++ prueba.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+def fibo1(a, b):
+    if a == 0:
+        print(b)
+    
+    secuencia = a + b
+    print(secuencia)
+    sleep(2)
+    fibo2(secuencia, b)
+
+def fibo2(c, d):
+    secuencia = c + d
+    print(secuencia)
+    fibo1(d, secuencia)
+
+def main():
+    fibo1(0,1)
+
+if __name__ == "__main__":
+    main()
